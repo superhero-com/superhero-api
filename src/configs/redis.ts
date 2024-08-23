28a984847feec325c29f47fb784d56ef6042f7ec
Badi Ifaoui <badi.ifaoui@zixdev.com>
2024-08-23 10:05:06 +0100

    fix: redis unique identifier

--- src/configs/redis.ts
+++ src/configs/redis.ts
@@ -2,6 +2,7 @@ import * as Redis from 'ioredis';
 import 'dotenv/config';
 
 export const REDIS_CONFIG: Redis.RedisOptions = {
+  keyPrefix: process.env.AE_NETWORK_ID || 'ae_mainnet',
   host: process.env.REDIS_HOST || 'localhost',
   port: parseInt(process.env.REDIS_PORT) || 6379,
 };
