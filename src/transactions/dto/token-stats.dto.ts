3afbdecf71c48d6ee19d91494cc623713e1642b8
Badi Ifaoui <badi.ifaoui@zixdev.com>
2024-11-14 11:37:38 +0100

    feat: extend PriceMovementDto to include high/low dates, change metrics, and all-time price movement

--- src/transactions/dto/token-stats.dto.ts
+++ src/transactions/dto/token-stats.dto.ts
@@ -3,9 +3,20 @@ import { ApiProperty } from '@nestjs/swagger';
 export class PriceMovementDto {
   @ApiProperty()
   high: number;
+  @ApiProperty()
+  high_date: Date;
 
   @ApiProperty()
   low: number;
+  @ApiProperty()
+  low_date: Date;
+
+  @ApiProperty()
+  change: number;
+  @ApiProperty()
+  change_percent: number;
+  @ApiProperty()
+  change_direction: string;
 
   @ApiProperty()
   last_updated: Date;
@@ -20,4 +31,7 @@ export class TokenPriceMovementDto {
 
   @ApiProperty()
   past_30d: PriceMovementDto;
+
+  @ApiProperty()
+  all_time: PriceMovementDto;
 }
