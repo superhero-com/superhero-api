c73714c0986a9cf4f757c48b40048d36e554ecee
Badi Ifaoui <badi.ifaoui@zixdev.com>
2024-12-10 11:32:37 +0100

    refactor: simplify volume calculation by using the last mint entry

--- src/transactions/services/transaction.service.ts
+++ src/transactions/services/transaction.service.ts
@@ -185,9 +185,8 @@ export class TransactionService {
             total_supply,
           };
         }
-        volume = new BigNumber(
-          toAe(decodedData.find((data) => data.name === 'Mint').args[1]),
-        );
+        const mints = decodedData.filter((data) => data.name === 'Mint');
+        volume = new BigNumber(toAe(mints[mints.length - 1].args[1]));
         amount = new BigNumber(
           toAe(decodedData.find((data) => data.name === 'Buy').args[0]),
         );
