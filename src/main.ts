9b856b1959a5dc4a36b52d0255a3da76447852bb
Ivaylo Badinov <venimus@gmail.com>
2025-01-06 19:10:45 +0200

    chore(ci): add appropriate container names and repos

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Tokaen Scan')
-    .setDescription('The Tokaen Scan API')
+    .setTitle('WORD CRAFT Scan')
+    .setDescription('The WORD CRAFT Scan API')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
