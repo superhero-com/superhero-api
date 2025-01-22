bc90acbf47a692982fec640045bebfa2b9b6c336
Badi Ifaoui <badi.ifaoui@zixdev.com>
2025-01-22 21:46:18 +0100

    docs: add token categories support section to README and update contract definitions

--- README.md
+++ README.md
@@ -30,3 +30,27 @@ $ npm run start:dev
 # production mode
 $ npm run start:prod
 ```
+
+
+## Token Categories Support
+```js
+// src/configs/contracts.ts
+export const BCL_FACTORY: Record<INetworkTypes, IFactorySchema> = {
+  [NETWORK_ID_MAINNET]: {
+    address: 'ct_..',
+    categories: {},
+  },
+  [NETWORK_ID_TESTNET]: {
+    address: 'ct_..',
+    /**
+     * Define the categories you want the api to support, if not cat defined here, the API will serve all categories.
+     */
+    categories: { 
+      // 'CATEGORY-ak_..': {
+      //   name: 'CATEGORY',
+      //   allowed_name_length: '20',
+      // },
+    },
+  },
+};
+```
--- src/configs/contracts.ts
+++ src/configs/contracts.ts
@@ -28,6 +28,10 @@ export const BCL_CONTRACTS: ICommunityFactoryContracts = {
   ],
 };
 
+/**
+ * Define the categories you want the api to support for each network.
+ * If no categories are defined, the api will support all categories.
+ */
 export const BCL_FACTORY: Record<INetworkTypes, IFactorySchema> = {
   [NETWORK_ID_MAINNET]: {
     address: 'ct_2i4e5bXZbAsdjZZRikZEvBA1M8B5xn9shpVPQJiqUoNWDCPuUm',
@@ -38,7 +42,7 @@ export const BCL_FACTORY: Record<INetworkTypes, IFactorySchema> = {
     categories: {
       // 'ALPHA-ak_LMYXQ6mRKUwyMwrCuCZ2TmzUUySNmmjL2ehabSSXCx2W65uyE': {
       //   name: 'ALPHA',
-      //   description: 'Token Gating Contract (Testnet)',
+      //   allowed_name_length: '20',
       // },
     },
   },
