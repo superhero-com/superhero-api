c5424db565e5aa5a6ea290e38cd28f9a7097fe8d
Badi Ifaoui <badi.ifaoui@zixdev.com>
2025-01-31 13:29:30 +0100

    feat: add WORDS collection configuration to BCL_FACTORY in contracts

--- src/configs/contracts.ts
+++ src/configs/contracts.ts
@@ -11,8 +11,26 @@ import {
  */
 export const BCL_FACTORY: Record<INetworkTypes, ICommunityFactorySchema> = {
   [NETWORK_ID_MAINNET]: {
-    address: 'ct_2i4e5bXZbAsdjZZRikZEvBA1M8B5xn9shpVPQJiqUoNWDCPuUm',
-    collections: {},
+    address: 'ct_NV8W3LnriBtdsbivPAo4jGQs68gA7HBPXVjnMMysuXqNSeMXG',
+    collections: {
+      'WORDS-ak_b8N3csfrdb1PWLWeA5xbxSeeaKLFnBfkdVayLt7ZqDdFnSCaZ': {
+        id: 'WORDS-ak_b8N3csfrdb1PWLWeA5xbxSeeaKLFnBfkdVayLt7ZqDdFnSCaZ',
+        name: 'WORDS',
+        allowed_name_length: '20',
+        allowed_name_chars: [
+          {
+            SingleChar: [45],
+          },
+          {
+            CharRangeFromTo: [48, 57],
+          },
+          {
+            CharRangeFromTo: [65, 90],
+          },
+        ],
+        description: 'Tokenize a unique name with up to 20.',
+      },
+    },
   },
   [NETWORK_ID_TESTNET]: {
     address: 'ct_2UGLvS4zBMG6W7KEM46qgcaom7FXSNNx99pjeyU7AykXQcwzV2',
