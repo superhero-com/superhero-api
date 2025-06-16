e88ee7f2ecabd24533fb4ad6b349d00ea5622094
Badi Ifaoui <badi.ifaoui@zixdev.com>
2025-06-16 13:50:55 +0100

    refactor: replace full resync logic with syncTokenHolders method in FixHoldersService for improved efficiency

--- src/bcl/services/fix-holders.service.ts
+++ src/bcl/services/fix-holders.service.ts
@@ -152,77 +152,8 @@ export class FixHoldersService {
         .getCount();
 
       if (holdersCount < 1) {
-        await this.fullResyncHolders(token);
+        await this.syncTokenHolders(token);
       }
     }
-  }
-
-  async fullResyncHolders(token: Token) {
-    const data = await this.getAllHoldersData(token);
-
-    if (!data?.length) {
-      return;
-    }
-
-    // const addresses = data.map((item) => item.account_id);
-    // delete all holders for this token
-    await this.tokenHolderRepository.delete({
-      aex9_address: token.address,
-    });
-
-    const holders = data.map((item) => ({
-      id: `${item.account_id}_${token.address}`,
-      address: item.account_id,
-      balance: item.amount,
-      aex9_address: token.address,
-    }));
-
-    // update or insert holders
-    await this.tokenHolderRepository.insert(holders);
-
-    // update token holders count
-    await this.tokensRepository.update(token.sale_address, {
-      holders_count: holders.length,
-    });
-  }
-
-  private async getAllHoldersData(token: Token) {
-    const url = `${ACTIVE_NETWORK.middlewareUrl}/v3/aex9/${token.address}/balances?limit=100`;
-
-    const holdersData = [];
-    const holders = await this.getAllHoldersList(url);
-    for (const holder of holders) {
-      try {
-        const holderUrl = `${ACTIVE_NETWORK.middlewareUrl}/v3/aex9/${token.address}/balances/${holder.account_id}`;
-        const data = await fetchJson(holderUrl);
-        holdersData.push({
-          ...holder,
-          amount: data?.amount,
-        });
-      } catch (error: any) {
-        this.logger.error(
-          `Error pulling and updating account aex9 balances for ${holder.account_id}`,
-          error,
-          error.stack,
-        );
-      }
-    }
-
-    return holdersData;
-  }
-
-  private async getAllHoldersList(url: string): Promise<any[]> {
-    const mdwData = await fetchJson(url);
-
-    if (mdwData?.next) {
-      return [
-        ...mdwData.data,
-        ...(await this.getAllHoldersList(
-          `${ACTIVE_NETWORK.middlewareUrl}${mdwData.next}`,
-        )),
-      ];
-    }
-
-    return mdwData?.data || [];
   }
 }
