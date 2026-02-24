d306f4bcfece0e651efe68f432e1df4cdfc738f2
Badi Ifaoui <badi.ifaoui@zixdev.com>
2026-02-24 07:36:55 +0100

    feat(affiliation): add BCL Affiliation Analytics service and controller, along with new analytics views

--- src/affiliation/affiliation.module.ts
+++ src/affiliation/affiliation.module.ts
@@ -8,6 +8,8 @@ import { AffiliationCode } from './entities/affiliation-code.entity';
 import { Affiliation } from './entities/affiliation.entity';
 import { Invitation } from './entities/invitation.entity';
 import { OAuthService } from './services/oauth.service';
+import { BclAffiliationAnalyticsService } from './services/bcl-affiliation-analytics.service';
+import { BclAffiliationAnalyticsController } from './controllers/bcl-affiliation-analytics.controller';
 
 @Module({
   imports: [
@@ -15,9 +17,9 @@ import { OAuthService } from './services/oauth.service';
     AccountModule,
     TypeOrmModule.forFeature([Affiliation, AffiliationCode, Invitation]),
   ],
-  providers: [OAuthService],
+  providers: [OAuthService, BclAffiliationAnalyticsService],
   exports: [],
-  controllers: [AffiliationController, InvitationsController],
+  controllers: [AffiliationController, InvitationsController, BclAffiliationAnalyticsController],
 })
 export class AffiliationModule {
   //
