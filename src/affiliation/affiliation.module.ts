7e69274919374d8e8a32392562b90e63069bab5b
Badi Ifaoui <badi.ifaoui@zixdev.com>
2026-02-24 07:41:24 +0100

    feat(affiliation): add BCL Affiliation Tree service and controller for managing affiliation tree data

--- src/affiliation/affiliation.module.ts
+++ src/affiliation/affiliation.module.ts
@@ -9,7 +9,9 @@ import { Affiliation } from './entities/affiliation.entity';
 import { Invitation } from './entities/invitation.entity';
 import { OAuthService } from './services/oauth.service';
 import { BclAffiliationAnalyticsService } from './services/bcl-affiliation-analytics.service';
+import { BclAffiliationTreeService } from './services/bcl-affiliation-tree.service';
 import { BclAffiliationAnalyticsController } from './controllers/bcl-affiliation-analytics.controller';
+import { BclAffiliationTreeController } from './controllers/bcl-affiliation-tree.controller';
 
 @Module({
   imports: [
@@ -17,9 +19,9 @@ import { BclAffiliationAnalyticsController } from './controllers/bcl-affiliation
     AccountModule,
     TypeOrmModule.forFeature([Affiliation, AffiliationCode, Invitation]),
   ],
-  providers: [OAuthService, BclAffiliationAnalyticsService],
+  providers: [OAuthService, BclAffiliationAnalyticsService, BclAffiliationTreeService],
   exports: [],
-  controllers: [AffiliationController, InvitationsController, BclAffiliationAnalyticsController],
+  controllers: [AffiliationController, InvitationsController, BclAffiliationAnalyticsController, BclAffiliationTreeController],
 })
 export class AffiliationModule {
   //
